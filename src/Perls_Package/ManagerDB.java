b59b7ed01725055e1bd2abde1e415dc061ca8b6a
RiG <RiG@192.168.1.2>
2013-08-04 19:46:56 +0400

    add name hightlide

--- src/Perls_Package/ManagerDB.java
+++ src/Perls_Package/ManagerDB.java
@@ -11,54 +11,54 @@ import java.net.URLEncoder;
 
 public class ManagerDB {
     private final String setScriptURL = "http://lolperl.zz.mu/insert_perl_234b_658z_2.php";
-    
+
     public String setDB(String perl, String author){
         HttpURLConnection conn = null;
         String result="";
         String param;
         try {
-            param = "perl=" +                    
+            param = "perl=" +
                     URLEncoder.encode(perl, "UTF-8") +
                     "&author=" + URLEncoder.encode(author, "UTF-8");
         } catch (UnsupportedEncodingException ex) {
             return "Ошибка преобразования строки о_О";
         }
-        
+
         try {
             URL url = new URL(setScriptURL);
-            conn = (HttpURLConnection) url.openConnection();            
+            conn = (HttpURLConnection) url.openConnection();
             conn.setRequestMethod("POST");
             conn.setRequestProperty("User-Agent", "Java bot " + author);
             conn.setUseCaches (false);
             conn.setDoInput(true);
             conn.setDoOutput(true);
-            
+
             try (DataOutputStream wr = new DataOutputStream (
                     conn.getOutputStream ())) {
                 wr.writeBytes (param);
                 wr.flush ();
             }
-            
+
             // Проверка кода ответа
             int code=conn.getResponseCode();
             if (code==200) {
-                BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));              
-                String inputLine;        
+                BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                String inputLine;
                 while ((inputLine = in.readLine()) != null) {
                     result+=inputLine;
-                }                
+                }
                 in.close();
             } else {
                 // Тут обработаем код ошибки серевра
                 return "Ошибка сервера " + code + ". Не добавлено!";
-            }             
+            }
         } catch (IOException e) {
-            e.printStackTrace();   
+            e.printStackTrace();
             // Тут обрабатЫваем отсутствие инета, недоступность хоста и т.д.
-            return "Нет соединения с сервером, не добавлено!";     
+            return "Нет соединения с сервером, не добавлено!";
         } finally {
             if(conn != null) {
-                conn.disconnect(); 
+                conn.disconnect();
                 conn = null;
             }
         }
